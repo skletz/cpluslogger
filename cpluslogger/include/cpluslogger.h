2f35fe2e8483b3d68e346fef77d10ad507af30de
Sabrina Kletz <skletz@edu.aau.at>
2016-08-31 13:59:34 +0200

    add include file to use the source

---
+++ cpluslogger/include/cpluslogger.h
@@ -0,0 +1,11 @@
+#ifndef __clpluslogger_h__
+#define __clpluslogger_h__
+
+#include "../src/cplusLoggerDefaults.h"
+#include "../src/cplusLoggerGlobal.h"
+#include "../src/cplusLoggerMacros.h"
+#include "../src/DebugLevel.hpp"
+#include "../src/Logger.hpp"
+#include "../src/ReportingLevel.hpp"
+
+#endif //__clpluslogger_h__
